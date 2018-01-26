43b0d0a989920b4d53628dab36d06f1332501c1a
awood976 <35541753+awood976@users.noreply.github.com>
2018-01-25 23:59:44 -0800

    Import 'www' File
    added entire 'www' directory of wamp64 folder

---
+++ www/aussie.py
@@ -0,0 +1,4 @@
+#!C:/Python3.6/python.exe
+ 
+print("content-type: text/html\n\n" )
+print("AUSSIE AUSSIE AUSSIE, OI OI OI")
---
+++ www/hi.py
@@ -0,0 +1,4 @@
+#!C:/Python3.6/python.exe
+ 
+print("content-type: text/html\n\n" )
+print("<br><B>hello python</B>")

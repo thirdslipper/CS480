2e69c139429402f5f042d9229e38689645eb0d48
awood976 <35541753+awood976@users.noreply.github.com>
2018-02-08 21:00:50 -0800

    Update 2/8
    Added search through manual tree location input

--- CS480-PythonWithJS/static/js/recipe.js
+++ CS480-PythonWithJS/static/js/recipe.js
@@ -3,12 +3,27 @@
 //loader element <p>
 const preObject = document.getElementById("loader");
 
-//firebase object
-var dbRefObject = firebase.database().ref().child('Recipes');
+//default search term
+var searchTerm = "Recipes";
+
+//recipe search on 'enter' function
+var searchInput = document.getElementById("recipe_search");
+searchInput.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13)
+    document.getElementById("recipe_search_button").click();
+});
 
 //onclick function
 function displayFunction() {
+  //firebase object
+  var dbRefObject = firebase.database().ref().child(searchTerm);
   dbRefObject.on('value', snap => {
     preObject.innerHTML = JSON.stringify(snap.val(), null, 5);
   });
+}
+
+function searchRecipes() {
+  searchTerm = document.getElementById("recipe_search").value;
+  displayFunction();
 }

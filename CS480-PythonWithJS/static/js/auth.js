a5410abb221c67caf676c50aa4c825b764e1b574
Colin Koo <chkoo@cpp.edu>
2018-02-26 16:57:47 -0800

    Removed commented code

--- CS480-PythonWithJS/static/js/auth.js
+++ CS480-PythonWithJS/static/js/auth.js
@@ -1,15 +1,4 @@
-/*
-firebase.auth().onAuthStateChanged(function(user){
-  if (user) { //signed in
-    document.getElementById('firebaseui-auth-container').style.display = 'none';
-    document.getElementById('loaded').style.display = 'block';
-  }
-  else {
-    document.getElementById('firebaseui-auth-container').style.display = 'block';
-    document.getElementById('loaded').style.display = 'none';
-  }
-});
-*/
+
 
 (function() {
   const txtEmail = document.getElementById('login-username');

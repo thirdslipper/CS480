f431129d1b8fa04ba34f2bde0927537130ce1812
Colin Koo <chkoo@cpp.edu>
2018-02-26 16:56:18 -0800

    Moved stuff from backend-test.py to backend.py, commented out FirebaseUI dependency, added javascript-based login/signup/logout

--- CS480-PythonWithJS/static/js/auth.js
+++ CS480-PythonWithJS/static/js/auth.js
@@ -1,10 +1,67 @@
+/*
 firebase.auth().onAuthStateChanged(function(user){
   if (user) { //signed in
     document.getElementById('firebaseui-auth-container').style.display = 'none';
     document.getElementById('loaded').style.display = 'block';
   }
   else {
-      document.getElementById('firebaseui-auth-container').style.display = 'block';
+    document.getElementById('firebaseui-auth-container').style.display = 'block';
     document.getElementById('loaded').style.display = 'none';
   }
 });
+*/
+
+(function() {
+  const txtEmail = document.getElementById('login-username');
+  const txtPassword = document.getElementById('login-password');
+  const btnLogin = document.getElementById('login-button');
+  const btnSignUp = document.getElementById('signup-button');
+  const btnLogout = document.getElementById('logout-button');
+
+  btnLogin.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+      //sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+      //catch error
+    promise.catch(e => console.log(e.message));
+  });
+
+  btnSignUp.addEventListener('click', e => {
+      // does not check authenticity of email
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+      //sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+      //catch error
+    promise.catch(e => console.log(e.message));
+  });
+
+  btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+  });
+
+    //firebaseUser can be populated or null if not logged in
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    var forms = document.getElementsByClassName("sign-in-form");
+    var i;
+      //var loggedin = document.getElementsByClassName("signed-in");
+      //log if logged in
+    if (firebaseUser){
+      console.log("logged in", firebaseUser);
+      btnLogout.classList.toggle("btn-action-hide", false);
+      for (i = 0; i < forms.length; i++){
+        forms[i].style.display = "none";
+      }
+    } 
+    else {
+      console.log('not logged in');
+      btnLogout.classList.toggle("btn-action-hide", true);
+      for (i = 0; i < forms.length; i++){
+        forms[i].style.display = "inline";
+      }
+    }
+  });
+}());

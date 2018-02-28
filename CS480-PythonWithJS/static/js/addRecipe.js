fa613eb99335400539119ea5be0e6598e06c399b
Donovahkiin <32839360+Donovahkiin@users.noreply.github.com>
2018-02-27 20:12:04 -0800

    Submit recipes to Firebase!
    Form is finally, truly, 100% working.
    
    - Still needs to include username of person submitting it, once we have that.
    - If excess blank text fields are remaining on submission, it will encounter errors.
    - Possibly should validate this recipe title doesn't already exist, before overwriting.

--- CS480-PythonWithJS/static/js/addRecipe.js
+++ CS480-PythonWithJS/static/js/addRecipe.js
@@ -1,9 +1,17 @@
+var numberOfInstructions = 1;
 
 function newIngredient() {
 	var $group = $('#group-ingredients');
 
-	$group.append('<input type="text" name="ingredients1[]" placeholder="Ingredient Name">');
+	$group.append('<input type="text" name="ingredients1[]" placeholder="Ingredient Name"> ');
 	$group.append('<input type="text" name="ingredients2[]" placeholder="Quantity, and other details"><br>')
+}
+
+function newInstruction() {
+	var $group = $('#group-instructions');
+
+	numberOfInstructions += 1;
+	$group.append('<input type="text" name="instructions[]" placeholder="Step ' + numberOfInstructions + '"><br>')
 }
 
 function removeIngredient() {
@@ -14,14 +22,35 @@ function removeIngredient() {
 	$group.find('br:last').remove();
 }
 
+function removeInstruction() {
+	var $group = $('#group-instructions');
+
+	$group.find('input:last').remove();
+	$group.find('br:last').remove();
+	if(numberOfInstructions > 0)
+		numberOfInstructions -= 1; 
+}
+
 //write validation that a recipe with this name does not already exist.
 //by default, set() overwrites any data in that path.
-// include username and imageUrl
 
-function writeUserData(recipeName, ingredients1[], ingredients2[]) {
-  firebase.database().ref('Recipes/' + recipeName + '/Instructions').set({
-    //posted by: username,
-    //profile_picture : imageUrl
-	ingredients1[0]: ingredients2[0]
-  });
+function writeUserData(recipeName, ingredients1, ingredients2, instructions) {		// ingredients and instructions are node list objects
+	alert("in function call");
+	  
+	for(i = 0; i < ingredients1.length; i++)
+	{
+		firebase.database().ref('Recipes/' + recipeName + '/Ingredients').child(ingredients1[i].value).set(ingredients2[i].value);
+	}
+
+	for(i = 0; i < instructions.length; i++)
+	{
+		var temp = "Instruction " + (i+1);
+		firebase.database().ref('Recipes/' + recipeName + '/Instructions').child(temp).set(instructions[i].value);
+	}
+
+	var user = firebase.auth().currentUser;
+	//firebase.database().ref('Recipes/' + recipeName).child("Posted by: ").set()
+
+	  //posted by: username,
+      //profile_picture : imageUrl
 }

ef7658297718f27d3d05ec35319175e190d1eea7
Colin Koo <chkoo@cpp.edu>
2018-03-04 18:31:16 -0800

    verification emails send

--- CS480-PythonWithJS/static/js/signup.js
+++ CS480-PythonWithJS/static/js/signup.js
@@ -5,6 +5,7 @@ var usersRef = dbRefObject.child('User Profiles/');
   const btnSignUp = document.getElementById('signup-button');
 
   btnSignUp.addEventListener('click', e => {
+    console.log('clicked button');
       // does not check authenticity of email
     var display = document.getElementById('displayname').value;
     var email = document.getElementById('login-username').value;
@@ -14,17 +15,17 @@ var usersRef = dbRefObject.child('User Profiles/');
     var lname = document.getElementById('lastname').value;
     var age = document.getElementById('age').value;
     //partial error checking, nonempty fields, numeric age, same passwords.
-    if (fname == '' || lname == '' || age == '') {
-      if (isNaN(age) && fname != '' && lname != '') {
-        alert("Age must be a number!");
+    if (fname == '' || lname == '' || age == '' || displayname == '') {
+      //proper error notification
+      if ((isNaN(age) || age <= 0) && fname != '' && lname != '') {
+        alert("Age must be a valid number!");
       } else {
         alert('Fill in all fields!');
-      }
-    }
-    else if (pass !== pass2) {
+      }    
+    } else if (pass !== pass2) {
+      console.log('pass doesn\'t match');
       alert('Passwords do not match!');
-    }
-    else if (!checkIfUserExists(display)){
+    } else if (!checkIfUserExists(userId)) {
         //sign in
       const promise = firebase.auth().createUserWithEmailAndPassword(email, pass);
         //catch error
@@ -33,12 +34,18 @@ var usersRef = dbRefObject.child('User Profiles/');
         alert(e);
       });
       promise.then(s => {
-        alert('Account created!');
+        var user = firebase.auth().currentUser;
+        alert('Account created, check your email for verification!');
+        user.sendEmailVerification().then(function() {
+          //sent email
+        }).catch(function(error) {
+          // error
+          console.log(error);
+        }); 
         storeUserDetails(display, fname, lname, email, age);
         document.location.href = '/profile.html';
       });
     }
-    
   });
 }());
 
@@ -67,3 +74,4 @@ function storeUserDetails(displayname, fname, lname, email, age) {
     Age: age
   });
 }
+
